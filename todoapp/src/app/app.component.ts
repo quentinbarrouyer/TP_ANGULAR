f37e901b3ba003fe59e66eb2d99355d68021fda3
quentin <b18001457>
2023-03-02 14:07:09 +0100

    liste des taches

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,32 +1,30 @@
+// Le décorateur Component permet de donner à Angular des informations
+// supplémentaires sur une classe afin d'expliquer que :
+// 1. C'est un composant ;
+// 2. Il devra afficher un template HTML donné ;
+// 3. Il aura des styles scopés ;
+// Et beaucoup d'autres choses encore
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div id="app">
+        <h1>La Todo App</h1>
+
+        <main>
+          <app-todo-list 
+            [tasks]="tasks"
+          ></app-todo-list>
+        </main>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'todoapp';
+  tasks = [
+    { id: 1, text: "Aller faire des courses", done: false },
+    { id: 2, text: "Faire à manger", done: true },
+  ];
 }
